40479fc6069c7bcc710c6b501fe08713f0f5c750
BarbedMach <furkanozdemir.xyz@gmail.com>
2024-11-23 21:15:36 +0300

    tested tmdb api connection. it is working.

--- app/src/main/java/com/example/cinehive/dataclasses/Movie.kt
+++ app/src/main/java/com/example/cinehive/dataclasses/Movie.kt
@@ -1,10 +1,16 @@
 package com.example.cinehive.dataclasses
 
-data class MovieResponse(
-    val results: List<Movie>
+data class Movie(
+    val id: Int,
+    val title: String,
+    val overview: String,
+    val poster_path: String?,
+    val release_date: String
 )
 
-data class Movie(
-    val name: String,
-    val posterPath: String? = null
+data class MovieResponse(
+    val page: Int,
+    val results: List<Movie>,
+    val total_results: Int,
+    val total_pages: Int
 )
