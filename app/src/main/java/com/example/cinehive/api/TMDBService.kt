436967965ed4bb8f2f3b563e8ec4ddbfc3d61cfd
gokalpsagnak1 <gokalp.sagnak@ozu.edu.tr>
2024-11-30 17:46:52 +0300

    created a service to get similar movies

--- app/src/main/java/com/example/cinehive/api/TMDBService.kt
+++ app/src/main/java/com/example/cinehive/api/TMDBService.kt
@@ -3,6 +3,7 @@ package com.example.cinehive.api
 import com.example.cinehive.api.responses.MovieResponse
 import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TMDBService {
@@ -31,6 +32,14 @@ interface TMDBService {
     suspend fun searchMovies(
         @Query("api_key") apiKey: String,
         @Query("query") query: String,
+        @Query("language") language: String = "en-US",
+        @Query("page") page: Int = 1
+    ) : Response<MovieResponse>
+
+    @GET("movie/{movie_id}/similar")
+    suspend fun getSimilarMovies(
+        @Path("movie_id") movieId: Int,
+        @Query("api_key") apiKey: String,
         @Query("language") language: String = "en-US",
         @Query("page") page: Int = 1
     ) : Response<MovieResponse>
